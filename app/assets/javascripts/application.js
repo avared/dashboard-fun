3c6311f3cda67c4b93966aba82a58ae237e9f093
avared <avared@gmail.com>
2016-12-26 05:37:05 +0000

    Cleaned Up Menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
 //= require metisMenu
 //= require morris
 //= require raphael

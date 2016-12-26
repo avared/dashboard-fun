059f7ff67160a14c460d467ca903e62f2535883c
avared <avared@gmail.com>
2016-12-26 06:31:22 +0000

    users page renders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'dashboard#index' 
+  resources :users
 
-  
-  root 'dashboard#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
